4ec1a7c0ce826d3c76e97db4f80b365c061bcfd1
LQ157 <1574939790@qq.com>
2022-01-08 11:16:26 +0800

    Site updated: 2022-01-08 11:16:25

---
+++ js/envelope.js
@@ -0,0 +1,8 @@
+//messageboard为你的信封所在位置
+if (window.location.pathname.match('messageboard')) {
+var fancybox = document.getElementsByClassName('fancybox');
+var beforeimg = document.getElementById('beforeimg');
+var afterimg = document.getElementById('afterimg');
+beforeimg.setAttribute("data-lazy-src","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/beforenew.png");
+afterimg.setAttribute("data-lazy-src","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/afternew.png");
+}
